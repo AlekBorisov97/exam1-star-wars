28fe08fc07dd643f285b32e608d779906f56cfc7
Alek Borisov <alekborisov17@gmail.com>
2018-03-07 15:42:11 +0200

    Added the .cs files.

---
+++ Problem02/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Problem02//DICT
+{//planet name -> planet population -> attack type -> soldier count
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<long>> attackedPlanetData = new Dictionary<string, List<long>>();
+            Dictionary<string, List<long>> destroyedPlanetData = new Dictionary<string, List<long>>();
+            int n = int.Parse(Console.ReadLine());
+            string line;
+            string regexPattern = @"@([A-Z])([a-zA-Z]*)([^@\-!:>]?):(\d+)!([A|D])!->([0-9]+)";
+            int attackedPlanets = 0, destroyedPlanets = 0;
+            for (int i = 0; i < n; i++)
+            {
+                line = Console.ReadLine();
+                int count = 0;
+                foreach (char item in line)
+                {
+                    if (item == 's' || item == 't' || item == 'a' || item == 'r' || item == 'S' || item == 'T' || item == 'A' || item == 'R')
+                    {
+                        count++;
+                    }
+                }
+                StringBuilder sb = new StringBuilder();
+                foreach (char item in line)
+                {
+                    sb.Append((char)(item - count));
+                }
+                string message = sb.ToString();
+                Match myMatch = Regex.Match(message, regexPattern);
+                if (myMatch.Groups.Count!=7)
+                {
+                    continue;
+                }
+                string planet = myMatch.Groups[1].ToString() + myMatch.Groups[2].ToString();
+                long population = long.Parse(myMatch.Groups[4].ToString());
+                string attackType = myMatch.Groups[5].ToString();
+                long soldierCount = long.Parse(myMatch.Groups[6].ToString());
+
+                if (attackType.Equals("A"))
+                {
+                    attackedPlanets++;
+                    if (!attackedPlanetData.ContainsKey(planet))
+                    {
+                        List<long> newList = new List<long> { population, soldierCount };
+                        attackedPlanetData.Add(planet, newList);
+                    }
+                    else
+                    {
+                        attackedPlanetData[planet][0] = population;
+                        attackedPlanetData[planet][0] = soldierCount;
+                    }
+                }
+                else if (attackType.Equals("D"))
+                {
+                    destroyedPlanets++;
+                    if (!destroyedPlanetData.ContainsKey(planet))
+                    {
+                        List<long> newList = new List<long> { population, soldierCount };
+                        destroyedPlanetData.Add(planet, newList);
+                    }
+                    else
+                    {
+                        destroyedPlanetData[planet][0] = population;
+                        destroyedPlanetData[planet][0] = soldierCount;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Attacked planets: {attackedPlanets}");
+            foreach (var pair in attackedPlanetData.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"-> {pair.Key}");
+            }
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets}");
+            foreach (var pair in destroyedPlanetData.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"-> {pair.Key}");
+            }
+        }
+    }
+}
---
+++ Problem03/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> sides = new Dictionary<string, string>();
+
+            string line;
+            string oneOfTheForces = string.Empty;
+            int oneOfTheForcesCount = 0;
+            int theOtherOfTheForcesCount = 0;
+            while ((line = Console.ReadLine())!= "Lumpawaroo")
+            {
+                string[] tokens = line.Split().ToArray();
+                string divider = tokens[1];
+
+                if (divider.Equals("|"))
+                {
+
+
+                    string[] elements = line.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string force = elements[0];
+                    string name = elements[1];
+                    oneOfTheForces = force;
+
+                    if (!sides.ContainsKey(name))
+                    {
+                        sides.Add(name, force);
+                    }
+                }
+                else if(divider.Equals("->"))
+                {
+                    string[] elements = line.Split("->").ToArray();
+                    string name = elements[0];
+                    string movesToForce = elements[1];
+
+                    if (!sides.ContainsKey(name))
+                    {
+                        sides.Add(name, movesToForce);
+                    }
+                    else
+                    {
+                        sides[name] = movesToForce;
+                        Console.WriteLine($"{name} joins the {movesToForce} side!");
+                    }
+                }
+            }
+
+
+
+
+            //COUNTING SIDES
+            foreach (var pair in sides)
+            {
+                if (pair.Value.Equals(oneOfTheForces))
+                {
+                    oneOfTheForcesCount++;
+                }
+                else
+                {
+                    theOtherOfTheForcesCount++;
+                }
+            }
+
+
+            //PRINT
+            string theOtherOfTheForces = string.Empty;
+            if ((oneOfTheForcesCount > theOtherOfTheForcesCount) || ((oneOfTheForcesCount==theOtherOfTheForcesCount)) && IsBigger(oneOfTheForces, theOtherOfTheForces))
+            {
+                Console.WriteLine($"Side: {oneOfTheForces.Trim()}, Members: {oneOfTheForcesCount}");
+                foreach (var pair in sides.OrderBy(x=>x.Key))
+                {
+                    if (pair.Value.Equals(oneOfTheForces))
+                    {
+                        Console.WriteLine($"!{pair.Key}");
+                    }
+                    else
+                    {
+                        theOtherOfTheForces = pair.Value;
+                    }
+                }
+                Console.WriteLine($"Side: {theOtherOfTheForces.Trim()}, Members: {theOtherOfTheForcesCount}");
+                foreach (var pair in sides.OrderBy(x => x.Key))
+                {
+                    if (!pair.Value.Equals(oneOfTheForces))
+                    {
+                        Console.WriteLine($"!{pair.Key}");
+                    }
+                }
+            }
+            else if ((oneOfTheForcesCount < theOtherOfTheForcesCount) || ( (oneOfTheForcesCount==theOtherOfTheForcesCount)) && IsBigger(theOtherOfTheForces, oneOfTheForces))
+            {
+                Console.WriteLine($"Side: {theOtherOfTheForces.Trim()}, Members: {theOtherOfTheForcesCount}");
+                foreach (var pair in sides.OrderBy(x => x.Key))
+                {
+                    if (!pair.Value.Equals(oneOfTheForces))
+                    {
+                        Console.WriteLine($"!{pair.Key}");
+                    }
+                }
+                Console.WriteLine($"Side: {oneOfTheForces.Trim()}, Members: {oneOfTheForcesCount}");
+                foreach (var pair in sides.OrderBy(x => x.Key))
+                {
+                    if (pair.Value.Equals(oneOfTheForces))
+                    {
+                        Console.WriteLine($"!{pair.Key}");
+                    }
+                    else
+                    {
+                        theOtherOfTheForces = pair.Value;
+                    }
+                }
+            }
+            
+
+        }
+
+        private static bool IsBigger(string IsItbigger, string theOther)
+        {
+            bool result = true;
+            for (int i = 0; i < Math.Min(IsItbigger.Length, theOther.Length); i++)
+            {
+                if (IsItbigger[i]<theOther[i])
+                {
+                    break;
+                }
+                else if(IsItbigger[i] == theOther[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
